88a5e0fd60683175bdc7ba3ee01a61be88749c38
cruise-commits <antonyomondi05@gmail.com>
2022-09-09 07:04:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**main - entry  point or simply main block**/
+/**
+ * main - entry  point or simply main block
+ * Return: Always 0(Success)
+*/
 int main(void)
 {
 int n;
