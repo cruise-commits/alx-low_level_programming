bf2ee9bbc8ecaefad5cef0772a54a49cb9c7bd8d
cruise-commits <antonyomondi05@gmail.com>
2022-09-10 00:00:16 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- * main - entry  point or simply main block
- * Return: Always 0(Success)
-*/
-int main(void)
-{
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%i is positive\n", n);
-else if (n < 0)
-printf("%i is negative\n", n);
-else
-printf("%i is zero\n", n);
-
-return (0);
-}
+#include <stdlib.h> 
+ #include <time.h> 
+ #include <stdio.h> 
+ /** 
+  * main - entry  point or simply main block 
+  * Return: Always 0(Success) 
+ */ 
+ int main(void) 
+ { 
+ int n; 
+  
+ srand(time(0)); 
+ n = rand() - RAND_MAX / 2; 
+ if (n > 0) 
+ printf("%i is positive\n", n); 
+ else if (n < 0) 
+ printf("%i is negative\n", n); 
+ else 
+ printf("%i is zero\n", n); 
+  
+ return (0); 
+ }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-0x01-Cvariable
+Task0
