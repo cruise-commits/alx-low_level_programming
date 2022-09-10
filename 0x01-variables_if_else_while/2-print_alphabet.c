b8f5b99218e0884f479a79d79f7bcfba6cf2aa37
cruise-commits <antonyomondi05@gmail.com>
2022-09-10 15:51:55 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - entry point.
  *
- * Return: Always 0 (Success)
+ * Return: Stops the program by returning 0.
  */
 int main(void)
 {
-<<<<<<< HEAD
-int num;
-num = 97;
-while (num <= 122)
-putchar(num);
-num++;
-=======
-int p;
-p = 97;
-while (p <= 122)
-{
-putchar(p);
-p++;
-}
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 putchar('\n');
+
 return (0);
 }
