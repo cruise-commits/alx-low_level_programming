741d51cd686a9cd47bf0328cc62563aa0f891f7a
cruise-commits <antonyomondi05@gmail.com>
2022-09-10 19:26:50 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - entry point.
+ * main - Prints the alphabet
  *
- * Return: Stops the program by returning 0.
+ * Return: Always (Success)
  */
 int main(void)
 {
-char letter;
+char c;
 
-for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
 putchar('\n');
-
 return (0);
 }
+
