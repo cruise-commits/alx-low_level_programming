c71987cab313b5cfd20f394ed1a6d545b3aa5424
cruise-commits <antonyomondi05@gmail.com>
2022-09-10 19:37:51 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase except letters q and e.
- *
- * Return: Always 0.
- */
+  * main - Prints some letters of alphabet
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-char letter;
+char c;
 
-for (letter = 'a'; letter <= 'z'; letter++)
+for (c = 'a'; c <= 'z'; c++)
 {
-if (letter != 'e' && letter != 'q')
-			putchar(letter);
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
 }
 
 putchar('\n');
 
 return (0);
 }
+
