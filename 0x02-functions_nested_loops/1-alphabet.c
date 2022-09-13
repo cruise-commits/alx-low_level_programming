e94e16804bf961d94d0d68274ebddc1594b013d1
cruise-commits <antonyomondi05@gmail.com>
2022-09-13 10:40:30 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,16 @@
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
+int main(void)
+{
 void print_alphabet(void)
 {
 char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
+  _putchar(letter);
 
 _putchar('\n');
+}
+ return (0);
 }
