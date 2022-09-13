1225a9db89e9d56878a0029fcfa33547e5d328d5
cruise-commits <antonyomondi05@gmail.com>
2022-09-13 10:49:53 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-void print_alphabet(void)
-{
-char letter;
+  void print_alphabet(void)
+  {
+    char letter;
 
-for (letter = 'a'; letter <= 'z'; letter++)
-  _putchar(letter);
+    for (letter = 'a'; letter <= 'z'; letter++)
+      _putchar(letter);
 
-_putchar('\n');
-}
- return (0);
+    _putchar('\n');
+  }
+  return (0);
 }
