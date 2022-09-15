4859987bd151f8d499fc4c7bf8ea99feceaf9ef2
cruise-commits <antonyomondi05@gmail.com>
2022-09-14 19:06:23 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
-#include "digit.h"
+#include "main.h"
 
 /**
- * add - Adds two integers.
- * @num1: The first integer.
- * @num2: The second integer.
- *
- * Return: The result of the addition.
- */
+* add - Adds two integers.
+* @num1: The first integer.
+* @num2: The second integer.
+*
+* Return: The result of the addition.
+*/
 int add(int num1, int num2)
 {
-return (num1 + num2);
+	return (num1 + num2);
 }
